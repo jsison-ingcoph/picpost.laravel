32fe42541fe6fea32ee1152cba78315f5880bf10
jsison-ingcoph <153572370+jsison-ingcoph@users.noreply.github.com>
2023-12-15 16:33:07 +0800

    User model login and register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -61,5 +62,57 @@ class UserController extends Controller
     public function destroy(User $user)
     {
         //
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+        $user =  User::where('username', $request->username)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'msg' => 'Invalid credentials.',
+            ], 401);
+        }
+        $token = $user->createToken('auth_token')->accessToken;
+        return response()->json([
+            'msg' => 'Login successfully.',
+            'token' => $token,
+        ], 200);
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $existingUser = User::where('username', $request->username)->first();
+            if ($existingUser) {
+                return response()->json([
+                    'msg' => 'Username already taken.',
+                ], 409);
+            }
+            $validated = $request->validate([
+                'username' => 'required|string|unique:users',
+                'password' => 'required|string',
+                'lastname' => 'required|string',
+                'firstname' => 'required|string',
+                'middlename' => 'nullable|string',
+                'suffix' => 'nullable|string',
+                'gender' => 'required|in:male,female',
+                'birthdate' => 'required|date',
+            ]);
+            $validated['password'] = Hash::make($validated['password']);
+            $user = User::create($validated);
+            return response()->json([
+                'msg' => 'Registered successfully.',
+                'user' => $user,
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'msg' => 'Internal server error.',
+                'err' => $th,
+            ], 500);
+        }
     }
 }
